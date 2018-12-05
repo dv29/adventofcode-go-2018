65caf8382e84e542ba082c6a79e19c67ec95b39a
Deep Vora <deep.vora@dporganizer.com>
2018-12-04 19:20:37 -0600

    d3p1 complete

---
+++ 03/01/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type ClaimRectangle struct {
+	id     uint
+	x      uint
+	y      uint
+	width  uint
+	height uint
+}
+
+func mapToClaimRectangle(str string, re *regexp.Regexp) (cr ClaimRectangle) {
+	value := re.FindStringSubmatch(str)
+	id, err := strconv.Atoi(value[1])
+	check(err)
+	x, err := strconv.Atoi(value[2])
+	check(err)
+	y, err := strconv.Atoi(value[3])
+	check(err)
+	width, err := strconv.Atoi(value[4])
+	check(err)
+	height, err := strconv.Atoi(value[5])
+	check(err)
+
+	return ClaimRectangle{
+		uint(id),
+		uint(x),
+		uint(y),
+		uint(width),
+		uint(height),
+	}
+}
+
+const FABRIC_SIZE = 1000
+
+func main() {
+	file, err := os.Open("./input.txt")
+	check(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	regex := regexp.MustCompile(`#(?P<id>\d+) @ (?P<x>\d+),(?P<y>\d+): (?P<width>\d+)x(?P<height>\d+)`)
+	var fabric [FABRIC_SIZE][FABRIC_SIZE]int
+	count := 0
+
+	for scanner.Scan() {
+		str := scanner.Text()
+		check(err)
+		cr := mapToClaimRectangle(str, regex)
+		for w := uint(0); w < cr.width; w++ {
+			for h := uint(0); h < cr.height; h++ {
+				val := fabric[cr.x+w][cr.y+h]
+				if val == 0 {
+					fabric[cr.x+w][cr.y+h] = int(cr.id)
+				} else if val != -1 {
+					fabric[cr.x+w][cr.y+h] = -1
+					count++
+				}
+			}
+		}
+	}
+
+	// for i := 0; i < FABRIC_SIZE; i++ {
+	// 	for j := 0; j < FABRIC_SIZE; j++ {
+	// 		fmt.Printf("\t%d\t", fabric[i][j])
+	// 	}
+	// 	fmt.Printf("\n")
+	// }
+
+	fmt.Printf("count: %d\n", count)
+
+	fmt.Println("")
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
