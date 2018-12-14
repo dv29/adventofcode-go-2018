042ab9cab67dfe4934037274a54de54b57a96c74
Deep Vora <deep.vora@dporganizer.com>
2018-12-13 23:50:36 -0600

    wip d6

--- 06/main.go
+++ 06/main.go
@@ -22,38 +22,16 @@ type Coord struct {
 }
 type Coords []Coord
 
-func (c Coords) PrintCoords() {
-	for _, x := range c {
-		log.Printf("%v\n", x)
-	}
-}
-
-func (c Coords) Plotter(grid [][]int) {
-	for index, x := range c {
-		grid[x.x][x.y] = index
-	}
-	// for x, _ := range grid {
-	// 	for y, _ := range grid[x] {
-	// 		fmt.Printf("%d", grid[x][y])
-	// 	}
-	// 	fmt.Println()
-	// }
-}
-
-func (c Coords) ImagePlotter(dc *gg.Context) {
-	for index, x := range c {
-		mx, my := float64(x.x), float64(x.y)
-		dc.DrawRectangle(mx*multiplier, my*multiplier, multiplier*2, multiplier*2)
-		ci := float64(index)
-		dc.SetRGB(ci, ci+mx/255.0, ci+my/255.0)
-		dc.Fill()
-	}
-}
-
 func stringToCoord(str string) Coord {
 	var coord Coord
 	fmt.Sscanf(str, "%d, %d", &coord.x, &coord.y)
 	return coord
+}
+
+func (c Coords) PrintCoords() {
+	for _, x := range c {
+		log.Printf("%v\n", x)
+	}
 }
 
 func (c Coords) GetBounds() (maxX, maxY int) {
@@ -70,6 +48,12 @@ func (c Coords) GetBounds() (maxX, maxY int) {
 	return
 }
 
+func (c Coords) Plotter(grid [][]int) {
+	for index, x := range c {
+		grid[x.x][x.y] = index + 1
+	}
+}
+
 func (c Coord) GetDistance(cb Coord) float64 {
 	return math.Abs(float64(c.x-cb.x)) + math.Abs(float64(c.y-cb.y))
 }
@@ -79,28 +63,47 @@ type MinCoord struct {
 	coordIndex int
 }
 
-func (coords Coords) PopulateDistanceMatrix(grid *[][]int) {
+func (coords Coords) PopulateDistanceMatrix(grid *[][]int) (m map[int]int) {
+	m = make(map[int]int)
 	for x, _ := range *grid {
 		for y, _ := range (*grid)[x] {
 			if (*grid)[x][y] == 0 {
 				min := MinCoord{
 					(Coord{x, y}).GetDistance(coords[0]),
-					0,
+					1,
 				}
 				for index, coord := range coords {
-					dist := (Coord{x, y}).GetDistance(coord)
-					if min.dist < dist {
+					if dist := (Coord{x, y}).GetDistance(coord); min.dist > dist {
 						min = MinCoord{
 							dist,
 							index,
 						}
 					} else if min.dist == dist {
-						(*grid)[x][y] = -1
+						min = MinCoord{
+							dist,
+							-1,
+						}
 					}
 				}
-				(*grid)[x][y] = min.coordIndex
+				(*grid)[x][y] = min.coordIndex + 1
+				if _, ok := m[min.coordIndex]; ok {
+					m[min.coordIndex] += 1
+				} else {
+					m[min.coordIndex] = 1
+				}
 			}
 		}
+	}
+	return
+}
+
+func (c Coords) ImagePlotter(dc *gg.Context) {
+	for index, x := range c {
+		mx, my := float64(x.x), float64(x.y)
+		dc.DrawRectangle(mx*multiplier, my*multiplier, multiplier*2, multiplier*2)
+		ci := float64(index)
+		dc.SetRGB(ci, ci+mx/255.0, ci+my/255.0)
+		dc.Fill()
 	}
 }
 
@@ -110,7 +113,7 @@ func PopulateImage(grid [][]int, dc *gg.Context) {
 			mx, my := float64(x), float64(y)
 			dc.DrawRectangle(mx*multiplier, my*multiplier, multiplier, multiplier)
 			ci := float64(grid[x][y])
-			dc.SetRGB(ci, 0, 0)
+			dc.SetRGB(ci, ci+ci/ci, ci+ci/ci)
 			dc.Fill()
 		}
 	}
@@ -122,6 +125,15 @@ func matrix(x, y int) [][]int {
 		m[i] = make([]int, y)
 	}
 	return m
+}
+
+func Max(m map[int]int) (max int) {
+	for _, x := range m {
+		if max < x {
+			max = x
+		}
+	}
+	return
 }
 
 func main() {
@@ -143,16 +155,17 @@ func main() {
 
 	grid := matrix(maxX+1, maxY+1)
 	coords.Plotter(grid)
+	m := coords.PopulateDistanceMatrix(&grid)
 
+	max := Max(m)
+	log.Printf("%d", max)
 	// mx, my := float64(maxX), float64(maxY)
 	dc := gg.NewContext(maxX*multiplier, maxY*multiplier)
 	// dc.DrawRectangle(0, 0, mx*multiplier, my*multiplier)
 	dc.SetRGB(255, 255, 255)
 	dc.Clear()
-	coords.ImagePlotter(dc)
-
-	coords.PopulateDistanceMatrix(&grid)
+	// coords.ImagePlotter(dc)
 	PopulateImage(grid, dc)
-	fmt.Printf("%v\n", grid)
+
 	dc.SavePNG("out.png")
 }
